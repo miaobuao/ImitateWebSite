7a9ac92f1a76a6d9575dd1cc02cee1eb35e12bfa
喵不嗷 <617802189@qq.com>
2022-12-07 14:45:40 +0800

    更新代码结构

--- demo.py
+++ demo.py
@@ -1,18 +1,4 @@
-##----ImitateWebsite-------##
-#     作者 : 喵不嗷          #
-#        第三方库            #
-#      bs4 , requests       #
-##-------------------------##
-        #==========#
-#===========================#
-        #==========#
-##----ImitateWebsite-------##
-#     Author : MiaoBuao     #
-#     Three-part libs：     #
-#      bs4，requests        #
-##-------------------------##
-
-
 import imi
+
 imitate=imi.Imitate(input("输入URL："), input("输入路径："))
 imitate.run()
--- imi.py
+++ imi.py
@@ -1,210 +1,198 @@
-##----ImitateWebsite-------##
-#     作者 : 喵不嗷          #
-#        第三方库            #
-#      bs4 , requests       #
-##-------------------------##
-#==========#
-#===========================#
-#==========#
-##----ImitateWebsite-------##
-#     Author : MiaoBuao     #
-#     Three-part libs：     #
-#      bs4，requests        #
-##-------------------------##
+"""
+Author: miaobuao
+repo:   https://github.com/miaobuao/ImitateWebSite
+mirror: https://gitee.com/miaobuao/ImitateWebSite
+"""
 
 import sys
+import meo
 import threading
 import re
 import os
-from bs4.element import PreformattedString
-import requests as r
+import requests
 from bs4 import BeautifulSoup as bs
-from urllib.parse import urlparse, uses_netloc
+from urllib.parse import urlparse
+
+
+def isHttps(https_url):
+    try:
+        if requests.get(https_url).status_code == 200:
+            return True
+    except:
+        return False
+
+
+def checkout_ssl(url):
+    info = urlparse(url)
+    path = ''
+    oldpath = info[2]
+    for i in oldpath.split("/"):
+        if i:
+            path += ('/'+i)
+    if not info[0]:
+        https = "https://"+info[1]+path
+        if isHttps(https):
+            return https
+        else:
+            return "http://"+info[1]+path
+    else:
+        return info[0]+"://"+info[1]+path
 
 
 class Download(threading.Thread):
-    def isHttps(self, https_url):
-        try:
-            if r.get(https_url).status_code == 200:
-                return True
-        except:
-            return False
 
-    def __init__(self):
-        super(Download, self).__init__()
-        self.path = ''
-        self.url = ''
-        self.r = ''
-
-    def fixUrl(self, url):
-        info = urlparse(url)
-        path = ''
-        oldpath = info[2]
-        for i in oldpath.split("/"):
-            if i:
-                path += ('/'+i)
-        if not info[0]:
-            https = "https://"+info[1]+path
-            if self.isHttps(https):
-                return https
-            else:
-                return "http://"+info[1]+path
-        else:
-            return info[0]+"://"+info[1]+path
+    path: str
+    url:  str
+    resp: requests.Response
 
     def add(self, url, path):
         self.path = path
-        self.url = self.fixUrl(url)
+        self.url = checkout_ssl(url)
         info = os.path.split(self.path)
         if not os.path.isdir(info[0]):
             os.makedirs(info[0])
-        self.r = r.get(self.url, stream=True)
-        # print(self.url+" :: "+self.path)
+        try:
+            self.resp = requests.get(self.url, stream=True)
+        except requests.exceptions.InvalidURL as e:
+            meo.screen.red_font(f"Error URL: {self.url}")
+            raise e
 
     def run(self):
+        if os.path.exists(self.path) and not os.path.isfile(self.path):
+            self.path = os.path.join(self.path, "index.html")
         with open(self.path, 'wb') as f:
-            for chunk in self.r.iter_content(1024):
+            for chunk in self.resp.iter_content(1024):
                 if chunk:
                     f.write(chunk)
 
 
+def isUrl(link):
+    if re.match('(http[s]?:)?//[0-9a-zA-Z-_]+\..+', link, re.M | re.I) == None:
+        return False
+    else:
+        return True
+
+
+def get_relative_path(dir):
+    if re.match("\./.+", dir) != None:
+        return dir
+    else:
+        return "./"+dir
+
+
+def checkout_dir(path):
+    info = os.path.split(path)
+    if not os.path.isdir(info[0]):
+        os.makedirs(info[0])
+
+def baseUrl(url):
+    url = checkout_ssl(url)
+    info = urlparse(url)[:3]
+    path = '/'+'/'.join(info[2].split("/")[:-1])+"/"
+    if path == '//':
+        path = "/"
+    return info[0]+"://"+info[1]+path
+
+
+def fixSrc(src):
+    lists = src.split("/")
+    words = []
+    for i in lists:
+        if i != '':
+            words.append(i)
+    return "/".join(words)
+    
+def fixPath(path):
+    info = os.path.split(path)
+    path = []
+    for i in info[0].split('/'):
+        if i != '':
+            path.append(i)
+    path.append(info[1])
+    return '/'.join(path)
 class Imitate:
 
-    def isUrl(self, link):
-        if re.match('(http[s]?:)?//[0-9a-zA-Z-_]+\..+', link, re.M | re.I) == None:
-            return False
-        else:
-            return True
-
+    @property
     def html(self):
-        self.text = self.r.text.encode(self.r.encoding).decode(self.encoding)
+        resp_enc = self.resp.encoding
+        text = self.resp.text.encode(resp_enc).decode(self.encoding)
+        return text
 
-    def rightDir(self, dir):
-        if(re.match("\./.+", dir) != None):
-            return dir
-        else:
-            return "./"+dir
+    @property
+    def content(self):
+        return self.resp.content
 
-    def __init__(self, url, root="./"):
+    @property
+    def doc(self):
+        doc = bs(self.content, features="lxml")
+        return doc
+
+    def __init__(self, url, root="./", encoding=sys.getdefaultencoding(), ignore_err=True):
         self.url = urlparse(url)
         self.oURL = url
-        self.root = self.rightDir(root)
+        self.root = get_relative_path(root)
         if not os.path.exists(self.root):
             os.makedirs(self.root)
         self.text = ''
-        self.doc = ''
-        self.r = r.get(url)
-        self.encoding = sys.getdefaultencoding()
-
-    def detectDir(self, path):
-        info = os.path.split(path)
-        if not os.path.isdir(info[0]):
-            os.makedirs(info[0])
-
-    def dom(self):
-        self.doc = bs(self.r.content, features="lxml")
-
-    def ishtml(self):
-        if not self.text:
-            self.html()
-
-    def getHtml(self):
-        self.isDom()
-        return self.doc.prettify()
+        self.resp = requests.get(url)
+        self.encoding = encoding
+        self.ignore_err = ignore_err
 
     def output(self, path, content):
-        self.detectDir(path)
+        checkout_dir(path)
         with open(path, "w", encoding=self.encoding) as f:
             f.write(content)
 
-    def outHtml(self, path):
-        self.isDom()
-        self.output(path, self.doc.prettify())
-
-    def isDom(self):
-        if not self.doc:
-            self.dom()
-
-    def fixSrc(self, src):
-        lists = src.split("/")
-        words = []
-        for i in lists:
-            if i != '':
-                words.append(i)
-        return "/".join(words)
-
-    def getContent(self, url):
-        return r.get(url).content
-
-    def baseUrl(self, url):
-        down = Download()
-        url = down.fixUrl(url)
-        info = urlparse(url)[:3]
-        path = '/'+'/'.join(info[2].split("/")[:-1])+"/"
-        if path == '//':
-            path = "/"
-        return info[0]+"://"+info[1]+path
-
-    def basePath(self, path):
-        return self.root+"/" + os.path.split(self.fixPath(path))[0]
-
-    def fixPath(self, path):
-        info = os.path.split(path)
-        path = []
-        for i in info[0].split('/'):
-            if i != '':
-                path.append(i)
-        path.append(info[1])
-        return '/'.join(path)
-
     def clone_tag(self, tag):
-        attrs = tag.attrs
-        if attrs["src"]:
-            if self.isUrl(attrs['src']):
-                src = attrs["src"]
-                info = urlparse(src)
-                if self.doc.find(tag.name, tag.attrs).attrs['src']:
-                    t = self.fixSrc(self.fixPath(info[1]+info[2]))
-                    self.doc.find(tag.name, tag.attrs).attrs['src'] = t
-                    print(t)
-                source = self.root+"/" + info[1]+info[2]
-            else:
-                src = self.baseUrl(self.oURL)+"/"+attrs['src']
-                if self.doc.find(tag.name, tag.attrs).attrs['src']:
-                    t = self.fixSrc(self.fixPath(urlparse(src)[2]))
-                    self.doc.find(tag.name, tag.attrs).attrs['src'] = t
-                    print(t)
-                source = self.root+"/"+urlparse(src)[2]
-        elif attrs["href"]:
-            if self.isUrl(attrs['href']):
-                src = attrs["href"]
-                info = urlparse(src)
-                if self.doc.find(tag.name, tag.attrs).attrs['href']:
-                    t = self.fixSrc(self.fixPath(info[1]+info[2]))
-                    self.doc.find(tag.name, tag.attrs).attrs['href'] = t
-                    print(t)
-                source = self.root + "/" + info[1] + info[2]
-            else:
-                src = self.baseUrl(self.oURL)+"/"+attrs['href']
-                if self.doc.find(tag.name, tag.attrs).attrs['href']:
-                    t = self.fixSrc(self.fixPath(urlparse(src)[2]))
-                    self.doc.find(tag.name, tag.attrs).attrs['href'] = t
-                    print(t)
-                source = self.root+"/"+urlparse(src)[2]
-        down = Download()
-        down.add(src, source)
-        down.run()
+        try:
+            attrs = tag.attrs
+        except AttributeError as e:
+            return
 
-    def run(self):
-        self.isDom()
-        for tag in self.doc.descendants:
+        if src := attrs.get("src", None):
+            src_type = "src"
+        elif src := attrs.get("href", None):
+            src_type = 'href'
+        else:
+            return
+            
+        src = src.strip() # type: str
+        if isUrl(src):
+            info = urlparse(src)
+            tar = fixSrc(fixPath(info[1]+info[2]))
+            source = self.root+"/" + info[1]+info[2]
+        else:
+            tar = src
+            source = self.root + "/" + src
+            src = baseUrl(self.oURL)+"/"+src
+        tag.attrs[src_type] = tar
+        print(tar)
+
+        try:
+            down = Download()
+            down.add(src, source)
+            down.run()
+        except requests.exceptions.InvalidURL as e:
+            print(tag)
+            print(e)
+            exit()
+
+    def clone_doc(self, doc):
+        for tag in doc.descendants:
             try:
                 self.clone_tag(tag)
-            except:
-                pass
+            except Exception as e:
+                if not self.ignore_err:
+                    raise e
+        return doc
+
+    def run(self):
+        doc = self.doc
+        doc = self.clone_doc(doc)
+
         if os.path.split(urlparse(self.oURL)[2])[1] == '':
             outpath = self.root+'/'+"index.html"
-            self.output(outpath, self.doc.prettify())
+            self.output(outpath, doc.prettify())
         else:
             outpath = self.root+'/'+os.path.split(urlparse(self.oURL)[2])[1]
-            self.output(outpath, self.doc.prettify())
+            self.output(outpath, doc.prettify())
